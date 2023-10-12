d6713fc9b7433222f3048ff370f640f17e02b388
pretzL <ericpretzinger@gmail.com>
2023-10-12 21:59:28 +0200

    Add watchable API

---
+++ src/js/utils/api.js
@@ -0,0 +1,245 @@
+const moviesDb = "https://moviesdatabase.p.rapidapi.com";
+const moviesDbKey = import.meta.env.VITE_MOVIESDATABASE_KEY;
+
+export const api = {
+    getAllTitles: async (params = "") => {
+        try {
+            const req = await fetch(`${moviesDb}/titles${params}`, {
+                method: "GET",
+                headers: {
+                    "X-RapidAPI-Key": moviesDbKey,
+                    "X-RapidAPI-Host": "moviesdatabase.p.rapidapi.com",
+                },
+            });
+            const json = await req.json();
+            return json;
+        } catch (error) {
+            throw new Error(error);
+        }
+    },
+    getATitle: async (id, params = "") => {
+        try {
+            const req = await fetch(`${moviesDb}/titles/${id}${params}`, {
+                method: "GET",
+                headers: {
+                    "X-RapidAPI-Key": moviesDbKey,
+                    "X-RapidAPI-Host": "moviesdatabase.p.rapidapi.com",
+                },
+            });
+            const json = await req.json();
+            return json;
+        } catch (error) {
+            throw new Error(error);
+        }
+    },
+    getRandomTitle: async (params = "") => {
+        try {
+            const req = await fetch(`${moviesDb}/titles/random${params}`, {
+                method: "GET",
+                headers: {
+                    "X-RapidAPI-Key": moviesDbKey,
+                    "X-RapidAPI-Host": "moviesdatabase.p.rapidapi.com",
+                },
+            });
+            const json = await req.json();
+            return json;
+        } catch (error) {
+            throw new Error(error);
+        }
+    },
+    getTitleRating: async (id, params = "") => {
+        try {
+            const req = await fetch(`${moviesDb}/titles/${id}/ratings${params}`, {
+                method: "GET",
+                headers: {
+                    "X-RapidAPI-Key": moviesDbKey,
+                    "X-RapidAPI-Host": "moviesdatabase.p.rapidapi.com",
+                },
+            });
+            const json = await req.json();
+            return json;
+        } catch (error) {
+            throw new Error(error);
+        }
+    },
+    getSeries: async (id, params = "") => {
+        try {
+            const req = await fetch(`${moviesDb}/titles/series/${id}${params}`, {
+                method: "GET",
+                headers: {
+                    "X-RapidAPI-Key": moviesDbKey,
+                    "X-RapidAPI-Host": "moviesdatabase.p.rapidapi.com",
+                },
+            });
+            const json = await req.json();
+            return json;
+        } catch (error) {
+            throw new Error(error);
+        }
+    },
+    getSeasonsNumber: async (id, params = "") => {
+        try {
+            const req = await fetch(`${moviesDb}/titles/seasons/${id}${params}`, {
+                method: "GET",
+                headers: {
+                    "X-RapidAPI-Key": moviesDbKey,
+                    "X-RapidAPI-Host": "moviesdatabase.p.rapidapi.com",
+                },
+            });
+            const json = await req.json();
+            return json;
+        } catch (error) {
+            throw new Error(error);
+        }
+    },
+    getEpisodes: async (id, season, params = "") => {
+        try {
+            const req = await fetch(`${moviesDb}/titles/series/${id}/${season}${params}`, {
+                method: "GET",
+                headers: {
+                    "X-RapidAPI-Key": moviesDbKey,
+                    "X-RapidAPI-Host": "moviesdatabase.p.rapidapi.com",
+                },
+            });
+            const json = await req.json();
+            return json;
+        } catch (error) {
+            throw new Error(error);
+        }
+    },
+    getEpisode: async (id, params = "") => {
+        try {
+            const req = await fetch(`${moviesDb}/titles/episode/${id}${params}`, {
+                method: "GET",
+                headers: {
+                    "X-RapidAPI-Key": moviesDbKey,
+                    "X-RapidAPI-Host": "moviesdatabase.p.rapidapi.com",
+                },
+            });
+            const json = await req.json();
+            return json;
+        } catch (error) {
+            throw new Error(error);
+        }
+    },
+    getUpcomingTitles: async (params = "") => {
+        try {
+            const req = await fetch(`${moviesDb}/titles/x/upcoming${params}`, {
+                method: "GET",
+                headers: {
+                    "X-RapidAPI-Key": moviesDbKey,
+                    "X-RapidAPI-Host": "moviesdatabase.p.rapidapi.com",
+                },
+            });
+            const json = await req.json();
+            return json;
+        } catch (error) {
+            throw new Error(error);
+        }
+    },
+    search: async (mode, text, params = "") => {
+        try {
+            const req = await fetch(`${moviesDb}/titles/search/${mode}/${text}${params}`, {
+                method: "GET",
+                headers: {
+                    "X-RapidAPI-Key": moviesDbKey,
+                    "X-RapidAPI-Host": "moviesdatabase.p.rapidapi.com",
+                },
+            });
+            const json = await req.json();
+            return json;
+        } catch (error) {
+            throw new Error(error);
+        }
+    },
+    getActors: async (params = "") => {
+        try {
+            const req = await fetch(`${moviesDb}/actors${params}`, {
+                method: "GET",
+                headers: {
+                    "X-RapidAPI-Key": moviesDbKey,
+                    "X-RapidAPI-Host": "moviesdatabase.p.rapidapi.com",
+                },
+            });
+            const json = await req.json();
+            return json;
+        } catch (error) {
+            throw new Error(error);
+        }
+    },
+    getActor: async (id, params = "") => {
+        try {
+            const req = await fetch(`${moviesDb}/actors/${id}${params}`, {
+                method: "GET",
+                headers: {
+                    "X-RapidAPI-Key": moviesDbKey,
+                    "X-RapidAPI-Host": "moviesdatabase.p.rapidapi.com",
+                },
+            });
+            const json = await req.json();
+            return json;
+        } catch (error) {
+            throw new Error(error);
+        }
+    },
+    getRandomActor: async (params = "") => {
+        try {
+            const req = await fetch(`${moviesDb}/actors/random${params}`, {
+                method: "GET",
+                headers: {
+                    "X-RapidAPI-Key": moviesDbKey,
+                    "X-RapidAPI-Host": "moviesdatabase.p.rapidapi.com",
+                },
+            });
+            const json = await req.json();
+            return json;
+        } catch (error) {
+            throw new Error(error);
+        }
+    },
+    getTitleTypes: async (params = "") => {
+        try {
+            const req = await fetch(`${moviesDb}/titles/utils/titleTypes${params}`, {
+                method: "GET",
+                headers: {
+                    "X-RapidAPI-Key": moviesDbKey,
+                    "X-RapidAPI-Host": "moviesdatabase.p.rapidapi.com",
+                },
+            });
+            const json = await req.json();
+            return json;
+        } catch (error) {
+            throw new Error(error);
+        }
+    },
+    getGenreTypes: async (params = "") => {
+        try {
+            const req = await fetch(`${moviesDb}/titles/utils/genres${params}`, {
+                method: "GET",
+                headers: {
+                    "X-RapidAPI-Key": moviesDbKey,
+                    "X-RapidAPI-Host": "moviesdatabase.p.rapidapi.com",
+                },
+            });
+            const json = await req.json();
+            return json;
+        } catch (error) {
+            throw new Error(error);
+        }
+    },
+    getTitlesList: async (params = "") => {
+        try {
+            const req = await fetch(`${moviesDb}/titles/utils/lists${params}`, {
+                method: "GET",
+                headers: {
+                    "X-RapidAPI-Key": moviesDbKey,
+                    "X-RapidAPI-Host": "moviesdatabase.p.rapidapi.com",
+                },
+            });
+            const json = await req.json();
+            return json;
+        } catch (error) {
+            throw new Error(error);
+        }
+    },
+};
