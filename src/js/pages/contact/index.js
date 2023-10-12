424a7f1ce4b81fb2903d9f916461b0d820d52993
Eric Pretzinger <ericpretzinger@gmail.com>
2023-10-12 14:50:38 +0200

    Add bs contact form

--- src/js/pages/contact/index.js
+++ src/js/pages/contact/index.js
@@ -1,6 +1,17 @@
 import { createNewElement } from "../../utils";
 
 export function buildContact() {
-    const div = createNewElement("div", { className: "test-class", style: { backgroundColor: "red" }, onclick: () => alert("Clicked!"), textContent: "Contact page!" });
-    return div;
+    const form = createNewElement("form", {
+        className: "form",
+        style: { display: "flex", flexDirection: "column", gap: "1rem", backgroundColor: "grey", padding: "1rem", borderRadius: "5px", width: "80%", margin: "0 auto" },
+    });
+    const nameInput = createNewElement("input", { className: "form-input", placeholder: "Name", type: "text" });
+    const emailInput = createNewElement("input", { className: "form-input", placeholder: "Email", type: "email" });
+    const messageInput = createNewElement("textarea", { className: "form-input", placeholder: "Message", style: { resize: "none", height: "150px" } });
+
+    form.appendChild(nameInput);
+    form.appendChild(emailInput);
+    form.appendChild(messageInput);
+
+    return form;
 }
